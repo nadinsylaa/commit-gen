df908ca9ee1302d6a84f9ec4d7edc4987f6ceb31
Nadins <77990703+nadinsylaa@users.noreply.github.com>
2022-01-23 10:44:34 +0700

    Update task.sh

--- task.sh
+++ task.sh
@@ -14,6 +14,6 @@ d=`date '+%Y-%m-%dT%H:%M:%SZ'`
 
 echo "## 🤔 LAST UPDATED AT: ${d}" > update.md
 
-git config --local user.email "regidesoftian@gmail.com"
-git config --local user.name "Egii"
+git config --local user.email "asepmlnaaa@gmail.com"
+git config --local user.name "Nadins"
 git commit -am "${arr[$rand]} (at ${d})"
